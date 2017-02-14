821a30ba44b0eb3e543b741d371d299b8f37da1f
elvismt <elvismtt@gmail.com>
2017-02-14 19:58:00 -0300

    Fixed static files urls

--- ewald/urls.py
+++ ewald/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url, include
 from . import views 
 
 urlpatterns = [
-    url(r'^login', views.login, name='login'),
-    url(r'^home', views.home, name='home'),
-    url(r'', views.default, name='default'),
+    url(r'^$', views.default, name='default'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^home/$', views.home, name='home'),
 ]
 
