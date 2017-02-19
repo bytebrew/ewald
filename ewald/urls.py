92096fed7499e69fea4a47eeb4a3b34853fdedc5
elvismt <elvismtt@gmail.com>
2017-02-19 18:36:40 -0300

    Added correct ajax call

--- ewald/urls.py
+++ ewald/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.default, name='default'),
-    url(r'^login/$', views.login, name='login'),
     url(r'^home/$', views.home, name='home'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
 
--- ewald/views.py
+++ ewald/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def default(request):
-    # TODO: check if user is logged in
     return redirect('/login')
 
 def login(request):
-    return render(request, 'ewald/login.html')
+    if request.method == 'POST':
+        print('Received POST:', request.POST)
+        return HttpResponse('')
+    else:
+        return render(request, 'ewald/login.html')
+
+def signup(request):
+    return render(request, 'ewald/signup.html')
 
 def home(request):
     return render(request, 'ewald/home.html')
